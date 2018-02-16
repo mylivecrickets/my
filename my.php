ea14e678308cb7de84fe8d01a40f5d269d5915e1
mylivecrickets <36165860+mylivecrickets@users.noreply.github.com>
2018-02-16 12:12:46 +0530

    Create my.php

---
+++ my.php
@@ -0,0 +1,104 @@
+
+
+              
+      
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<body>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>VVcast.tv - Embedded player</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<link type="text/css" href="http://azcast.live/css/embed.css?ver=1.4.4" rel="stylesheet" media="screen" />  
+<style>
+body {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  margin:0;
+  padding:0;
+}
+.hdplayer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100% !important;
+  100%;
+  min-height: 400px;
+  overflow: hidden;
+}
+
+#counter {
+			padding: 5px 0;
+			text-align: center;
+			color: #fff;
+			background: #000;
+		}
+
+
+</style>
+</head>
+<body>
+   
+<span style='display:none' id=SatBfregiehctiukns>rxsDTR-9MRkh3AlIrFC8aUn8EbpDnOhnZAwp99U4on_3c==s</span><span style='display:none' id=gcBaitesfniethrkuS>EaUUnD=83nl4=D9kTnrC8nIZr9hbRA-9o_ORA3xhMcswpspF</span><span style='display:none' id=ciukteraftegiSBnsh>TpRs4FUMpUC-3rAoDbw8938=Ernkl_hOnAxR=nDIcZha9s9n</span><span style='display:none' id=cuetkerisnfiaBgtSh>Fp9rDRhrhsRI3s93UbCMwn4UE8Aa=_l=nnAZnpOok8cD9x-T</span><span style='display:none' id=enukSriesatgBchtif>obD88x4whE9RZ33nA=O=9sFlhrcIDkUCRAnpn9MpT-nasrU_</span><span style='display:none' id=ncBSiifeukresagtht>Zr-xb=CsaOMpADw8nnrUh93_=lphnRUI9Tsnc38DE4kF9oRA</span><span style='display:none' id=rtBhctSgsnkiuaefie>UrFcp8=9bk3U9xnhCwp8AlEaRhn9M4Z_sO3sn-DrnIRAD=oT</span><span style='display:none' id=iBurfetnsehScaktgi>4R9IDsAlRoDO8_rFCspnnMAU3n9TaUZpn=h89c3rE=wbh-kx</span><span style='display:none' id=isaniuBSktftehcegr>x8_TI9UDZwn-FEnUnRcrOh3sa439nrhlsCk9MpA=oD8Ap=bR</span><span style='display:none' id=BteSrgfnechaiksuit>U=rpOnb8Awspa4hRoTCn-kUcA_=rFR3xD9El3nIh9M89ZsDn</span><span style='display:none' id=fttiaSeekuhircgBsn>x=rInbOnT-_A4pwlpakMrFRonUhUZ8n93C83DhcARs9D=9sE</span><span style='display:none' id=SegtrsneuBhitkfcai>AMsrTZCDnIl=9ROn=x49ArUah3s-E_h8RFp9n3opwcD8bkUn</span><span style='display:none' id=tgiSeercaikfhsButn>xnpRr==_CcEA8MRsnInhU9OD8ThZFlosUpD4A-n93abwr93k</span><span style='display:none' id=iheskaicetgrBfSunt>noAsDnsh=9UnlhRn39krp-Ow=4cDCaxR9rU_AbEF88ZI3MpT</span><span style='display:none' id=BkticfSratisuengeh>93RcwUbZsMnI-l_rFDOphTnonsD8xAarp=AUC4hnE9Rk98=3</span><span style='display:none' id=asugarcniShtftBeik>ash=_rRsEnM3Z9boUlprn9xFp-OI9whD4nCA3UDnkT8Rc8A=</span><span style='display:none' id=BtaheiuktecsfgSrin>UAn98Rb4ArD-anprIc==9Dp9n_nM3E3sTxwO8hlFkChsZRoU</span><span style='display:none' id=kigehuansicfeStrBt>OZD934pn3Ia8Mks9=9nhUnhTxADAcEnbrp_oR=8ClwUF-srR</span><span style='display:none' id=githfsStaiBeukrnec>pR9l8sDphha=3UrE9OUw9s4MnxAFARknc8nnbCoT3DZ=I-_r</span><span style='display:none' id=ntciiskefeSruBhtag>abORhC=ZEpRU99Th_893MsFnknUDso4xIp=c-n8ADA3rrnwl</span><span style='display:none' id=ufhSketsragtBnieci>RrwapM9A9O_ExUI4TnD-3F=hRnAkos8bZnrnl3cDChU8sp=9</span><span style='display:none' id=uikaeBftrheistSgcn>pUnnkonA3c9w_UC3=OEAnMFpD8Dhlb9I4r=xsT9RahR-8Zrs</span><span style='display:none' id=hitnsSrutfecakgiBe>8ZDnwRpsc9Tnk=UnOA_=lbI4hERDCUr93Mx9rsp8Aa3onFh-</span><span style='display:none' id=hkcsirfattBneigeuS>F_whT=rbpOR8C8xRl9n9DnU9sckD=rn-nEhZos43IMpAaUA3</span><span style='display:none' id=sigtaehSfuntBrkcei>A9ZUch=a-poCbAD_ThnO9UMs8nI8Rk=nl3pwERr3sFnDx9r4</span><span style='display:none' id=iireSsgtfahtuBcnke>3Is-9M=na9D9CTZsRRhx4nw8E_Alo3rO8=DpncpAbkhnUFrU</span><span style='display:none' id=ainBSkhfteutcseirg>3oxR9rshRh_nMZlcUAn98=8p9U=ksDrAODn-3a4CEnIFTbwp</span><span style='display:none' id=iBhuetsraiSknfctge>nxwRD8_UFbA4n3hak8OIArDsTUMrC=39c9pnhlsRoZE-=n9p</span><span style='display:none' id=tnteuSeachBkirfigs>OhpnoAwnA8p=8sUFEnrUhTZ3C=3RaxR4sb-9McklrInD9D9_</span><span style='display:none' id=ihiBeaugctnrtskeSf>E9pr9nn8ZClpDDh3ckArTnOhso=sR3FaMU=AnRUw4b-xI89_</span><script src="https://content.jwplatform.com/libraries/IDzF9Zmk.js"></script>
+<div id='mediaplayer'></div>
+<script type="text/javascript">
+var igaryesSrleuAnUrtbear = ["&hl","hls","518","sh","764","tim","end","art","sst","e=0","e=1","tim","&hl","040"];
+var aerbiulegsnASUryaterr = ["end","e=1","sst","518","tim","040","&hl","&hl","764","tim","art","sh","hls","e=0"];
+var rriUySgAbalerrasnteue = ["&hl","&hl","tim","sst","e=1","art","tim","764","hls","e=0","040","518","sh","end"];
+var tsuerSArnerUriebagyla = ["hls","sh","040","764","art","sst","&hl","tim","end","e=0","518","e=1","tim","&hl"];
+var brSrauilnUseetrygaAre = ["art","end","518","&hl","tim","e=0","040","sst","e=1","&hl","sh","tim","764","hls"];
+var UaaeySlerbrtuniregsAr = ["e=0","040","tim","764","&hl","e=1","hls","518","end","&hl","tim","sh","sst","art"];
+var AeregrnsuSiaeUbartyrl = ["&hl","art","sh","518","sst","&hl","e=1","764","hls","040","tim","end","tim","e=0"];
+var AlnuSteigUeerarbysarr = ["e=0","tim","end","040","&hl","hls","&hl","518","764","sst","sh","art","tim","e=1"];
+var SebtyrUilrgernraseaAu = ["&hl","e=1","art","040","518","tim","764","hls","sh","sst","e=0","end","&hl","tim"];
+var eurnlrtieersAaSUgraby = ["tim","&hl","art","sh","e=1","040","518","sst","&hl","tim","end","764","e=0","hls"];
+var eeubsartarryeglSArUin = ["&hl","764","tim","040","art","518","e=0","e=1","tim","sst","end","hls","&hl","sh"];
+var lryneuearsegStaiArrUb = ["sst","hls","art","e=1","end","040","e=0","tim","tim","518","&hl","764","&hl","sh"];
+var rrrSaglentrAeuyebasUi = ["040","end","hls","e=1","e=0","&hl","sst","&hl","tim","518","art","764","tim","sh"];
+var rlraenUbreiASrstuegay = ["sst","tim","040","art","&hl","e=0","&hl","hls","tim","end","764","518","e=1","sh"];
+var igUraneasytruerAeblSr = ["&hl","end","e=0","sh","&hl","040","sst","hls","518","e=1","tim","tim","764","art"];
+var euSryUnaegAsarribtarl = ["hls","end","tim","e=1","518","764","040","&hl","sst","art","tim","e=0","&hl","sh"];
+var larUysbuneirSrgteraeA = ["tim","hls","sst","&hl","518","end","764","tim","art","e=1","e=0","040","&hl","sh"];
+var byrAtrrleegSearUisanu = ["sh","&hl","art","&hl","e=0","tim","tim","764","040","518","end","sst","e=1","hls"];
+var sSangerbilAyeeatrruUr = ["tim","&hl","e=1","&hl","tim","e=0","hls","sst","764","sh","518","end","art","040"];
+var AsSgauaryrUberrinlete = ["sst","tim","&hl","518","&hl","tim","e=0","764","art","040","hls","e=1","end","sh"];
+var UeygiealburtrAernsSar = ["art","e=1","sst","end","sh","tim","764","&hl","&hl","tim","e=0","040","hls","518"];
+var iearlretbSrAugryesnUa = ["art","e=1","tim","040","&hl","tim","sst","hls","&hl","e=0","764","sh","end","518"];
+var rrnAbiyrgaUsauetrSele = ["518","e=1","sh","sst","&hl","040","end","hls","764","&hl","tim","tim","art","e=0"];
+var lauArrsUeenerrabyitgS = ["art","e=1","sh","&hl","e=0","hls","764","end","518","tim","sst","tim","040","&hl"];
+var rtrarbaeASireUnysuleg = ["tim","sh","&hl","art","040","hls","end","e=0","&hl","tim","764","sst","e=1","518"];
+var rbUrasrrgetSuanileAye = ["tim","hls","&hl","art","040","sst","&hl","e=1","tim","sh","764","e=0","end","518"];
+var ieelbgasrtyAUnrSruare = ["hls","040","tim","tim","e=0","art","&hl","end","e=1","518","764","sst","&hl","sh"];
+var ernsbSrrgieUalrtayeAu = ["end","tim","040","764","sh","hls","e=1","sst","e=0","&hl","tim","&hl","518","art"];
+var ueiybrrAaUrresalngeSt = ["518","art","&hl","e=1","tim","764","&hl","tim","sst","sh","hls","040","e=0","end"];
+var rSareulsirgbAeyantUer = ["518","764","sst","tim","sh","040","hls","&hl","art","&hl","e=1","e=0","end","tim"];
+var playerInstance = jwplayer("mediaplayer");
+playerInstance.setup({
+    playlist: [{
+         sources: [{ 
+            provider: "rtmp",
+            file: pRrtglmUet()        },{
+            file: HrUlttgpte()        }]
+    }],
+        primary: "flash",
+       height: 400,
+   width: 600,
+          aspectratio: "16:9",
+    autostart: "true",
+         title: "Live Streaming",
+         autostart: "true",
+         repeat: "true",
+         mute: false
+});
+     function HrUlttgpte() {
+        return(["h","t","t","p",":","\/","\/","1","8","5",".","3","9",".","1","0",".","7","1",":","1","9","3","5","\/","t","v","\/","C","B","h","z","1","2","1","g","Q","R","2","0","1","7","1","1","0","5","\/","p","l","a","y","l","i","s","t",".","m","3","u","8","?"].join("") + euSryUnaegAsarribtarl.join("") + document.getElementById("asugarcniShtftBeik").innerHTML);
+    }
+ function pRrtglmUet() {
+        return(["r","t","m","p",":","\/","\/","1","8","5",".","3","9",".","1","0",".","7","1",":","1","9","3","5","\/","t","v","\/","C","B","h","z","1","2","1","g","Q","R","2","0","1","7","1","1","0","5","?"].join("") + euSryUnaegAsarribtarl.join("") + document.getElementById("asugarcniShtftBeik").innerHTML);
+    }
+</script>
+
